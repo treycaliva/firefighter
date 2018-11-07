3dde01abb8b5c77517539615f84fe92fc09ec61b
phoet <phoetmail@googlemail.com>
2018-11-07 10:28:08 +0100

    cleanup loose ends

--- lib/firefighter.rb
+++ lib/firefighter.rb
@@ -1,3 +1,4 @@
 require "firefighter/version"
+require "firefighter/web"
 require "firefighter/realtime_database"
 require "firefighter/token_generator"
---
+++ lib/firefighter/web.rb
@@ -0,0 +1,25 @@
+require 'logger'
+require 'json'
+require 'http'
+
+module Firefighter
+  module Web
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
+    def call(method, url, data = {})
+      if method == :get
+        response = HTTP.get(url, headers: {'Content-Type' => 'application/json'})
+      else
+        response = HTTP.send(method, url, headers: {'Content-Type' => 'application/json'}, body: JSON.dump(data))
+      end
+      if response.status == 200
+        JSON.parse(response.body)
+      else
+        @logger.warn "firebase #{method} failed #{url} #{response.body}"
+        raise "firebase #{method} failed #{url} #{response.body}"
+      end
+    end
+  end
+end
